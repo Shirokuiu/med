59114e05b32141574fe8058f1081825b96f02b69
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-10-04 10:56:25 +0300

    Добавил поддержку svg спрайтов в теле html + добавил префиксы css для старых браузеров

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'always',
-    printWidth: 110,
-    semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 110,
+  semi: true,
 };
