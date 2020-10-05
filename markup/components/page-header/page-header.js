8340f68ad3762174d54544738cf9b561285715a0
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-10-05 20:33:27 +0300

    Сделал футер

--- markup/components/page-header/page-header.js
+++ markup/components/page-header/page-header.js
@@ -1,8 +1,9 @@
 const MOBILE_WIDTH = 1160;
+const $body = document.body;
 const $header = document.querySelector('.page-header');
 const $rowTop = $header.querySelector('#rowTop');
 const $topList = $header.querySelector('#topList');
-const $logo = $header.querySelector('.page-header__logo-wrap');
+const $logo = $header.querySelector('#pageHeaderLogo');
 const $tel = $header.querySelector('.page-header__tel-wrap');
 const $menuBtn = $header.querySelector('.page-header__top-menu-btn');
 const $mobileMenu = $header.querySelector('.page-header__mobile-menu');
@@ -16,6 +17,7 @@ if (window.innerWidth <= MOBILE_WIDTH) {
 }
 
 $menuBtn.addEventListener('click', () => {
+  $body.classList.toggle('page--menu-open');
   $menuBtn.classList.toggle('page-header__top-menu-btn--open');
   $mobileMenu.classList.toggle('page-header__mobile-menu--open');
 });
