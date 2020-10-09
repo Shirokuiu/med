57cf28977643c6dc8e3debcd633ebffd35f0acd8
Павел Осипов <pavelosipov@iMac-Pavel.local>
2020-10-09 15:17:23 +0300

    Сделал правки

--- markup/components/services/services.js
+++ markup/components/services/services.js
@@ -9,6 +9,32 @@ export default class Services {
 
   init() {
     Helpers.isMobileWidth(this._MAX_WIDTH, () => {
+      const $tables = document.querySelectorAll('.services__table');
+
+      if ($tables) {
+        [...$tables].forEach(($table) => {
+          const $tableTrs = $table.querySelectorAll('.services__tr');
+          const $thForm = $table.querySelector('.services__head-th--form');
+          const $thPrice = $table.querySelector('.services__head-th--price');
+
+          if ($tableTrs) {
+            [...$tableTrs].forEach(($tableTr) => {
+              const $tdTitle = $tableTr.querySelector('.services__td--title');
+              const tdForm = $tableTr.querySelector('.services__td--form');
+              const tdPrice = $tableTr.querySelector('.services__td--price');
+              const prices = [...tdPrice.querySelectorAll('.services__td-text')].map(
+                (priceText) => priceText.textContent
+              );
+
+              $tdTitle.insertAdjacentHTML(
+                'beforeend',
+                this._getMobileTemplate($thForm.textContent, $thPrice.textContent, tdForm.textContent, prices)
+              );
+            });
+          }
+        });
+      }
+
       [...this._$sliderContainer].forEach((sliderContainer) => {
         sliderContainer.classList.add('owl-carousel');
       });
@@ -24,5 +50,20 @@ export default class Services {
         });
       });
     });
+  }
+
+  _getMobileTemplate(thForm, thPrice, tdForm, prices) {
+    return `
+      <ul class="services__td-modile-list">
+        <li class="services__td-mobile-items">
+          <span class="services__td-mobile-title">${thForm}</span>
+          <p class="services__td-mobile-text">${tdForm}</p>
+        </li>
+         <li class="services__td-mobile-items">
+            <span class="services__td-mobile-title">${thPrice}</span>
+            ${prices.map((price) => `<p class="services__td-mobile-text">${price}</p>`).join('')}
+         </li>
+      </ul>
+    `.trim();
   }
 }
